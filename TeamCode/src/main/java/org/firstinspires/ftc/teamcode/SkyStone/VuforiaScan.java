013b7b9fd79506a21130c58c4abbd0ae3aba95da
Alek <aleksakers@gmail.com>
2019-10-01 16:47:15 -0400

    Adding new team resources

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SkyStone/VuforiaScan.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SkyStone/VuforiaScan.java
@@ -35,6 +35,9 @@ import static org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocaliz
 public class VuforiaScan extends LinearOpMode {
     
     private String configuration = "Not Decided";
+
+    private static final double leftBound = 0;
+    private static final double rightBound = 0;
     
     private static final VuforiaLocalizer.CameraDirection CAMERA_CHOICE = BACK;
     private static final boolean PHONE_IS_PORTRAIT = false;
@@ -171,73 +174,8 @@ public class VuforiaScan extends LinearOpMode {
         
         //drive around, move foundation, etc
 
-        CameraDevice.getInstance().setFlashTorchMode(true);
 
-        // Note: To use the remote camera preview:
-        // AFTER you hit Init on the Driver Station, use the "options menu" to select "Camera Stream"
-        // Tap the preview window to receive a fresh image.
-
-        targetsSkyStone.activate();
-        while (!isStopRequested() && configuration == "Not Decided") {
-
-
-            
-            // check all the trackable targets to see which one (if any) is visible.
-            targetVisible = false;
-            for (VuforiaTrackable trackable : allTrackables) {
-                if (((VuforiaTrackableDefaultListener)trackable.getListener()).isVisible()) {
-                    telemetry.addData("Visible Target", trackable.getName());
-                    
-                    
-                    if(trackable.getName().equals("stoneTarget")){
-                        targetVisible = true;
-
-                    }
-
-                       
-                    
-
-                    // getUpdatedRobotLocation() will return null if no new information is available since
-                    // the last time that call was made, or if the trackable is not currently visible.
-                    OpenGLMatrix robotLocationTransform = ((VuforiaTrackableDefaultListener)trackable.getListener()).getUpdatedRobotLocation();
-                    if (robotLocationTransform != null) {
-                        lastLocation = robotLocationTransform;
-                    }
-                    break;
-                }
-                
-            }
-
-            // Provide feedback as to where the robot is located (if we know).
-            if (targetVisible) {
-                
-                //grab it, move it where it goes, come back and start agian
-                
-                
-                // express position (translation) of robot in inches.
-                VectorF translation = lastLocation.getTranslation();
-                telemetry.addData("Pos (in)", "{X, Y, Z} = %.1f, %.1f, %.1f",
-                        translation.get(0) / mmPerInch, translation.get(1) / mmPerInch, translation.get(2) / mmPerInch);
-
-                // express the rotation of the robot in degrees.
-                Orientation rotation = Orientation.getOrientation(lastLocation, EXTRINSIC, XYZ, DEGREES);
-                telemetry.addData("Rot (deg)", "{Roll, Pitch, Heading} = %.0f, %.0f, %.0f", rotation.firstAngle, rotation.secondAngle, rotation.thirdAngle);
-           
-                
-            if(translation.get(0) <= leftBound){
-                configuration = "A"; 
-              }else if(translation.get(0) >= leftBound && translation.get(0) <= rightBound){
-                configuration = "B";
-              }else if(translation.get(0) >= rightBound){
-                configuration = "C";
-              }
-
-           }
-            else {
-                telemetry.addData("Visible Target", "none");
-            }
-            telemetry.update();
-        }
+        scan(targetsSkyStone, allTrackables);
 
         // Disable Tracking when we are done;
         targetsSkyStone.deactivate();
@@ -249,7 +187,80 @@ public class VuforiaScan extends LinearOpMode {
         // stuff after collection
         
     }
-    
+
+
+    private void scan(VuforiaTrackables targetsSkyStone, List<VuforiaTrackable> allTrackables){
+
+        CameraDevice.getInstance().setFlashTorchMode(true);
+
+        // Note: To use the remote camera preview:
+        // AFTER you hit Init on the Driver Station, use the "options menu" to select "Camera Stream"
+        // Tap the preview window to receive a fresh image.
+
+        targetsSkyStone.activate();
+        while (!isStopRequested() && configuration == "Not Decided") {
+
+
+
+            // check all the trackable targets to see which one (if any) is visible.
+            targetVisible = false;
+            for (VuforiaTrackable trackable : allTrackables) {
+                if (((VuforiaTrackableDefaultListener)trackable.getListener()).isVisible()) {
+                    telemetry.addData("Visible Target", trackable.getName());
+
+
+                    if(trackable.getName().equals("stoneTarget")){
+                        targetVisible = true;
+
+                    }
+
+
+
+
+                    // getUpdatedRobotLocation() will return null if no new information is available since
+                    // the last time that call was made, or if the trackable is not currently visible.
+                    OpenGLMatrix robotLocationTransform = ((VuforiaTrackableDefaultListener)trackable.getListener()).getUpdatedRobotLocation();
+                    if (robotLocationTransform != null) {
+                        lastLocation = robotLocationTransform;
+                    }
+                    break;
+                }
+
+            }
+
+            // Provide feedback as to where the robot is located (if we know).
+            if (targetVisible) {
+
+                //grab it, move it where it goes, come back and start agian
+
+
+                // express position (translation) of robot in inches.
+                VectorF translation = lastLocation.getTranslation();
+                telemetry.addData("Pos (in)", "{X, Y, Z} = %.1f, %.1f, %.1f",
+                        translation.get(0) / mmPerInch, translation.get(1) / mmPerInch, translation.get(2) / mmPerInch);
+
+                // express the rotation of the robot in degrees.
+                Orientation rotation = Orientation.getOrientation(lastLocation, EXTRINSIC, XYZ, DEGREES);
+                telemetry.addData("Rot (deg)", "{Roll, Pitch, Heading} = %.0f, %.0f, %.0f", rotation.firstAngle, rotation.secondAngle, rotation.thirdAngle);
+
+
+                if(translation.get(0) <= leftBound){
+                    configuration = "A";
+                }else if(translation.get(0) >= leftBound && translation.get(0) <= rightBound){
+                    configuration = "B";
+                }else if(translation.get(0) >= rightBound){
+                    configuration = "C";
+                }
+
+            }
+            else {
+                telemetry.addData("Visible Target", "none");
+            }
+            telemetry.update();
+        }
+
+
+    }
 
 
     // can be adapted later to collect skyStones from other group of three.
