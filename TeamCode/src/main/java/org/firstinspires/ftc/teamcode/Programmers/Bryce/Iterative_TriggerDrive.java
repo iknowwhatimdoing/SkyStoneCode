f51b5364a16681a4f2a2957c86fa40c451d138f4
Bryce <superbryce007@gmail.com>
2019-10-04 19:46:12 -0400

    Practiced different forms of stopping and movements.
    Added a cool Message.
    
    Changed Outreach Drive Folder
    
    Added Trigger Drive

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Programmers/Bryce/Iterative_TriggerDrive.java
@@ -0,0 +1,70 @@
+package org.firstinspires.ftc.teamcode.Programmers.Bryce;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@TeleOp(name="Iterative OpMode", group="Iterative Opmode")
+@Disabled
+public class Iterative_TriggerDrive extends OpMode
+{
+
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor leftDrive;
+    private DcMotor rightDrive;
+
+
+    @Override
+    public void init() {
+        telemetry.addData("Status", "Initialized");
+
+
+        leftDrive  = hardwareMap.get(DcMotor.class, "left_drive");
+        rightDrive = hardwareMap.get(DcMotor.class, "right_drive");
+
+
+        leftDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightDrive.setDirection(DcMotor.Direction.REVERSE);
+
+
+        telemetry.addData("Status", "Initialized");
+    }
+
+
+    @Override
+    public void init_loop() {
+    }
+
+
+    @Override
+    public void start() {
+        runtime.reset();
+    }
+
+
+    @Override
+    public void loop() {
+
+        double leftPower;
+        double rightPower;
+
+        leftPower = gamepad1.left_trigger + rightDrive;
+        rightPower = gamepad1.right_trigger - rightDrive;
+
+
+        leftDrive.setPower(leftPower);
+        rightDrive.setPower(rightPower);
+
+        telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
+        telemetry.addData("Status", "Run Time: " + runtime.toString());
+
+    }
+
+
+    @Override
+    public void stop() {
+    }
+
+}
